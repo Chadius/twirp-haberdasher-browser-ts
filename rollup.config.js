1a391dfd671fe163051874c63f0805553c796fdc
Chad Serrant <therealchadius@gmail.com>
2022-01-11 11:26:07 -0500

    Going to try vitejs.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import nodeResolve from "@rollup/plugin-node-resolve";
+import {nodeResolve} from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs"
 
 const baseConfig = createBasicConfig();
 
@@ -10,12 +10,8 @@ export default merge(baseConfig, {
     input: './src/client.ts',
     output: {
         dir: 'dist',
-        sourcemap: true,
-        format: "iife"
+        format: 'iife',
+        sourcemap: true
     },
-    plugins: [
-        commonjs(),
-        nodeResolve(),
-        typescript(),
-    ]
+    plugins: [nodeResolve(), typescript(), commonjs()]
 });
